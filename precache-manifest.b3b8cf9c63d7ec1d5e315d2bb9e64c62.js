43e3c0fa0b48bec8d2ef3647d1044983cb4bc151
wellflag <itcwang@yahoo.co>
2020-04-14 20:58:53 -0500

    AAA

---
+++ precache-manifest.b3b8cf9c63d7ec1d5e315d2bb9e64c62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bbe760e2101cb57da97e0d6ad353dbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14070eef01be32729418",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "334c221c9c6f0d0c15ce",
+    "url": "/static/js/2.9ddea6d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9ddea6d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14070eef01be32729418",
+    "url": "/static/js/main.348ab900.chunk.js"
+  },
+  {
+    "revision": "572b652b81fd377208e7",
+    "url": "/static/js/runtime-main.9e5a10f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
